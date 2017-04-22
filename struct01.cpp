86d6e428a6eee913d5f04ef808ff6373e073553f
glashunti <gleibsonglas2@yahoo.com.br>
2017-04-21 21:14:25 -0300

    First commit

---
+++ struct01.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+
+struct inflatable
+{
+	char name[20];
+	float volume;
+	double price;
+
+};
+
+int main()
+{
+	inflatable guest =
+	{
+		"Glorious Gloria",
+		1.88,
+		29.99
+	};
+    
+	inflatable pal =
+	{
+		"Audacious Arthur",
+		3.12,
+		32.99
+
+	};
+
+	// statc inflatable guest
+	std::cout << "Expand your guest list with " << guest.name;
+	std::cout << " and " << pal.name << "!\n";
+
+	//pal.name is the name member of the pal variable
+	std::cout << "You can have both fo $";
+	std::cout << guest.price + pal.price << "!\n";
+	
+	
+	return 0;
+	
+}
+
+
